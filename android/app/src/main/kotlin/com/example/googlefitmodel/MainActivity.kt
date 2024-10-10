3785fca051f5714cb30150caa8dffb2ffacf72ae
EHTESHAMURRAHMAN <ehteshamkhan2002@gmail.com>
2024-10-10 14:57:54 +0530

    working on health package

--- android/app/src/main/kotlin/com/example/googlefitmodel/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/googlefitmodel/MainActivity.kt
@@ -1,5 +1,10 @@
-package com.example.googlefitmodel
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
+package com.example.googlefitmodel  
+  
+import io.flutter.embedding.android.FlutterFragmentActivity  
+import android.os.Bundle  
+  
+class MainActivity: FlutterFragmentActivity() {  
+   override fun onCreate(savedInstanceState: Bundle?) {  
+      super.onCreate(savedInstanceState)  
+   }  
+}
